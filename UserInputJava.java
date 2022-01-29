4aa5f8df48463d70e1268a841c23ac17eb97d95f
Anirudha <51014110+anirudhagaikwad@users.noreply.github.com>
2022-01-29 17:49:33 +0530

    user input

---
+++ UserInputJava.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class UserInputJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Scanner scanObj=new Scanner(System.in);
+      int a=scanObj.nextInt();
+      String str= scanObj.nextLine();
+      float f= scanObj.nextFloat();
+      
+		
+	} // end main()
+
+} // end class UserInputJava
